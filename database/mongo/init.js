3d931a5bfbb1ba1d3444ba43ca871c8264f57da0
Hajar Berouag <berouaghajar.2003@gmail.com>
2026-03-17 10:36:36 +0100

    Update init.js

--- database/mongo/init.js
+++ database/mongo/init.js
@@ -2,14 +2,3 @@ db = db.getSiblingDB("hackathon");
 
 // RAW documents
 db.createCollection("documents_raw");
-
-// OCR text
-db.createCollection("documents_ocr");
-
-// extracted fields
-db.createCollection("documents_extracted");
-
-// logs
-db.createCollection("processing_logs");
-
-db.createCollection("ground_truth");
